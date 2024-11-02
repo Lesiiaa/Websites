90d1789c2f4c3bc425820fa02b457f6becec562d
Letycja <letycja.niemiec@gmail.com>
2024-11-02 12:52:18 +0100

    initial commit

---
+++ try_new_things/script.js
@@ -0,0 +1,32 @@
+function filterItems(tableId) {
+  const input = document.getElementById("searchInput");
+  const filter = input.value.toLowerCase();
+  const table = document.getElementById(tableId);
+  const rows = table.getElementsByTagName("tr");
+
+  // Iteruj przez wszystkie wiersze tabeli (poza naglowkiem)
+  for (let i = 1; i < rows.length; i++) {
+    const cells = rows[i].getElementsByTagName("td");
+    let found = false;
+
+    // sprawdz czy ktoras komorka zawiera szukany tekst
+    for (let j = 0; j < cells.length; j++) {
+      // mozliwosc szukania po danych atrybutach
+      if (
+        cells[j].textContent.toLowerCase().indexOf(filter) > -1 ||
+        (cells[j].dataset.categories &&
+          cells[j].dataset.categories.toLowerCase().indexOf(filter) > -1)
+      ) {
+        found = true; // jesli znajdziesz pasujacy tekst, ustaw found na true
+        break;
+      }
+    }
+
+    //pokaz lub ukryj wiersz w zaleznosci czy znaleziono szukana fraze
+    if (found) {
+      rows[i].style.display = ""; //pokaz
+    } else {
+      rows[i].style.display = "none"; //ukryj
+    }
+  }
+}
